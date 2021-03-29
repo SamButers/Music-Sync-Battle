fa7856e53d64b8f6d8fe99fa1fc1dd6466902fbb
Sam Buters <sambuters@gmail.com>
2021-03-29 13:32:14 -0300

    Adding new enemy events mapping

--- src/Enemy.gd
+++ src/Enemy.gd
@@ -1,118 +1,160 @@
 extends Node2D
 
-# General attack parameters
-export var shoot_frequency = 1;
-export var state = 0;
-
-# Cumulative sleeping bullets
-var generic_direction : float = 0.0;
-var generic_angle : float = 0.0;
-var generic_strength := Vector2(100, 200);
-var generic_damping : float = 0.1;
-var generic_bullet_array : Array = [];
-
-var time_elapsed = shoot_frequency;
-var previous_state = state;
+var sleeping_bullets : Dictionary = {};
 
 var bullet;
 var BulletTimer;
+var animation_player;
+var stage;
+
+var time_begin;
+var time_delay;
+
+var enemy_events;
 
 onready var player = get_parent().get_child(1);
 
-func shoot(strength, target, damping = 0, size = 1, random_start = false, random_end = false):
-	var new_bullet = bullet.instance();
-	new_bullet.position = Vector2(0, 0);
-	new_bullet.change_trajectory_through_direction(strength, get_angle_to(target), damping);
-	add_child(new_bullet);
-	
-func _wake_bullet_barrage(timer, target, strength):
-	if(typeof(target) != 5):
-		target = to_local(target.position);
-		
-	for bullet in timer.bullet_array:
-		if(bullet != null):
-			bullet.change_trajectory_through_normalization(rand_range(strength.x, strength.y), (target - bullet.position).normalized());
-		
-	remove_child(timer);
-	timer.queue_free();
-
-func bullet_sleeping_barrage(direction, angle, strength, target, target_strength = Vector2(600, 750), time = 0.833333, quantity = 30):
-	var timer = BulletTimer.new();
-	add_child(timer);
-	timer.connect("timeout", self, "_wake_bullet_barrage", [timer, target, target_strength]);
-	timer.one_shot = true;
-	timer.bullet_array = Array();
-	
-	for c in range(quantity):
-		var new_bullet = bullet.instance();
-		new_bullet.position = Vector2(0, 0);
-		new_bullet.change_trajectory_through_direction(rand_range(strength.x, strength.y), rand_range(direction - angle, direction + angle), 0.4);
-		timer.bullet_array.push_back(new_bullet);
-		add_child(new_bullet);
-		
-	timer.start(time);
-	
-func pattern_barrage(direction = -PI/2, arc_angle = PI, strength = 400, quantity = 30):
-	var angle_delta = arc_angle/quantity;
-	var current_angle = direction - arc_angle/2;
-	var maximum_angle = direction + arc_angle/2;
-	
-	while(current_angle < maximum_angle):
-		var new_bullet = bullet.instance();
-		new_bullet.position = Vector2(0, 0);
-		add_child(new_bullet);
-		new_bullet.change_trajectory_through_direction(strength, current_angle);
-		
-		current_angle += angle_delta;
-
-func wake_generic_bullets(target, strength):
-	for bullet in generic_bullet_array:
-		if(bullet != null):
-			bullet.change_trajectory_through_normalization(strength, (target - bullet.position).normalized());
-	generic_bullet_array.clear();
-
 func _ready():
+	set_process(false);
+	set_physics_process(false);
+	
+	var f = File.new();
+	f.open("res://src/scenes/stage1_enemy_events.json", File.READ);
+	
+	enemy_events = JSON.parse(f.get_as_text()).result;
+	
+	f.close();
+	
 	bullet = preload("res://src/scenes/Bullet.tscn");
 	BulletTimer = preload("res://src/BulletTimer.gd");
+	animation_player = get_child(2);
+	stage = get_parent();
+	animation_player.play("Intro");
+
+func _on_animation_finished(anim_name):
+	if(anim_name == "Intro"):
+		animation_player.play("Intro2");
+		time_begin = OS.get_ticks_usec();
+		time_delay = AudioServer.get_time_since_last_mix() + AudioServer.get_output_latency();
+		set_process(true);
+		set_physics_process(true);
+		stage.playBGM();
+
+#func shoot(strength, target, damping = 0, size = 1, random_start = false, random_end = false):
+#	var new_bullet = bullet.instance();
+#	new_bullet.position = Vector2(0, 0);
+#	new_bullet.change_trajectory_through_direction(strength, get_angle_to(target), damping);
+#	add_child(new_bullet);
+#
+#func _wake_bullet_barrage(timer, target, strength):
+#	if(typeof(target) != 5):
+#		target = to_local(target.position);
+#
+#	for bullet in timer.bullet_array:
+#		if(bullet != null):
+#			bullet.change_trajectory_through_normalization(rand_range(strength.x, strength.y), (target - bullet.position).normalized());
+#
+#	remove_child(timer);
+#	timer.queue_free();
+#
+#func bullet_sleeping_barrage(direction, angle, strength, target, target_strength = Vector2(600, 750), time = 0.833333, quantity = 30):
+#	var timer = BulletTimer.new();
+#	add_child(timer);
+#	timer.connect("timeout", self, "_wake_bullet_barrage", [timer, target, target_strength]);
+#	timer.one_shot = true;
+#	timer.bullet_array = Array();
+#
+#	for c in range(quantity):
+#		var new_bullet = bullet.instance();
+#		new_bullet.position = Vector2(0, 0);
+#		new_bullet.change_trajectory_through_direction(rand_range(strength.x, strength.y), rand_range(direction - angle, direction + angle), 0.4);
+#		timer.bullet_array.push_back(new_bullet);
+#		add_child(new_bullet);
+#
+#	timer.start(time);
+#
+#func pattern_barrage(direction = -PI/2, arc_angle = PI, strength = 400, quantity = 30):
+#	var angle_delta = arc_angle/quantity;
+#	var current_angle = direction - arc_angle/2;
+#	var maximum_angle = direction + arc_angle/2;
+#
+#	while(current_angle < maximum_angle):
+#		var new_bullet = bullet.instance();
+#		new_bullet.position = Vector2(0, 0);
+#		add_child(new_bullet);
+#		new_bullet.change_trajectory_through_direction(strength, current_angle);
+#
+#		current_angle += angle_delta;
+#
+#func wake_generic_bullets(target, strength):
+#	for bullet in generic_bullet_array:
+#		if(bullet != null):
+#			bullet.change_trajectory_through_normalization(strength, (target - bullet.position).normalized());
+#	generic_bullet_array.clear();
 
 func _physics_process(delta):
+	var time = (OS.get_ticks_usec() - time_begin) / 1000000.0;
+	time -= time_delay;
+	time = max(0, time);
+	
+	if(player == null):
+		get_tree().paused = true;
+	
+	while(enemy_events.size() && time >= enemy_events[0].time):
+		var ev = enemy_events.pop_front();
+		
+		if(ev.event == 'singleSleepingBullet'):
+			if(ev.type == 0):
+				if(!sleeping_bullets.has(ev.collection)):
+					sleeping_bullets[ev.collection] = [];
+					
+				var new_bullet = bullet.instance();
+				new_bullet.position = Vector2(0, 0);
+				new_bullet.change_trajectory_through_direction(rand_range(ev.data.strength.min, ev.data.strength.max),
+															   rand_range(ev.data.direction - ev.data.arc, ev.data.direction + ev.data.arc),
+															   ev.data.damping);
+				sleeping_bullets[ev.collection].push_back(new_bullet);
+				add_child(new_bullet)
+#		var callback = funcref(self, listeners_array[0][1]);
+#		callback.call_funcv(listeners_array[0][2]);
+	
 #	States
 #	State 0: Not moving
 #	State 1: Periodically shooting
 #	State 2: Charging sleeping bullets
 #	State 4: Laser
 
-	if(state == 1):
-		if(time_elapsed > 0):
-			time_elapsed -= delta;
-			
-		else:
-			time_elapsed = shoot_frequency;
-			pattern_barrage(get_angle_to(player.position), PI, 400, 30);
-		
-	elif(state == 2):
-		if(time_elapsed > 0):
-			time_elapsed -= delta;
-		else:
-			var new_bullet = bullet.instance();
-			new_bullet.position = Vector2(0, 0);
-			new_bullet.change_trajectory_through_direction(rand_range(generic_strength.x, generic_strength.y), rand_range(generic_direction - generic_angle, generic_direction + generic_angle), generic_damping);
-			generic_bullet_array.push_back(new_bullet);
-			add_child(new_bullet);
-			
-			time_elapsed = shoot_frequency;
-		
-	elif(state == 3):
-		pass
-		
-	elif(state == 4):
-		pass
-		
-	else:
-		if(time_elapsed > 0):
-			time_elapsed -= delta;
-			
-		else:
-			time_elapsed = shoot_frequency;
-			bullet_sleeping_barrage(-PI/2, PI/2, Vector2(100, 200), player);
-#			shoot(100, get_parent().get_child(1).position);
+#	if(state == 1):
+#		if(time_elapsed > 0):
+#			time_elapsed -= delta;
+#
+#		else:
+#			time_elapsed = shoot_frequency;
+#			pattern_barrage(get_angle_to(player.position), PI, 400, 30);
+#
+#	elif(state == 2):
+#		if(time_elapsed > 0):
+#			time_elapsed -= delta;
+#		else:
+#			var new_bullet = bullet.instance();
+#			new_bullet.position = Vector2(0, 0);
+#			new_bullet.change_trajectory_through_direction(rand_range(generic_strength.x, generic_strength.y), rand_range(generic_direction - generic_angle, generic_direction + generic_angle), generic_damping);
+#			generic_bullet_array.push_back(new_bullet);
+#			add_child(new_bullet);
+#
+#			time_elapsed = shoot_frequency;
+#
+#	elif(state == 3):
+#		pass
+#
+#	elif(state == 4):
+#		pass
+#
+#	else:
+#		if(time_elapsed > 0):
+#			time_elapsed -= delta;
+#
+#		else:
+#			time_elapsed = shoot_frequency;
+#			bullet_sleeping_barrage(-PI/2, PI/2, Vector2(100, 200), player);
+##			shoot(100, get_parent().get_child(1).position);
--- src/scenes/Stage1.gd
+++ src/scenes/Stage1.gd
@@ -2,73 +2,10 @@ extends Node2D
 
 var enemy;
 var player;
-var animationPlayer;
 
-var time_begin;
-var time_delay;
-
-onready var listeners_array : Array = [
-	[6.664, 'action1', []],
-	[13.331, 'action2', []],
-	[13.753, 'action3', [0]],
-	[14.580, 'action3', [1]],
-	[15.411, 'action3', [0]],
-	[16.246, 'action3', [1]],
-	[17.082, 'action3', [0]]
-];
+func playBGM():
+	$BGM.play();
 	
-func action1():
-	enemy.generic_direction = -PI/2;
-	enemy.generic_angle = PI/2;
-	enemy.generic_strength = Vector2(100, 200);
-	enemy.generic_damping = 0.5;
-	enemy.shoot_frequency = 0.2;
-	enemy.time_elapsed = 0;
-	enemy.state = 2;
-	
-func action2():
-	enemy.wake_generic_bullets(enemy.to_local(player.position), 500);
-	enemy.state = 3;
-	animationPlayer.play("Intro3");
-	
-func action3(side):
-	var direction = 2 * PI/3 if side else PI/3;
-	enemy.bullet_sleeping_barrage(direction, PI/2, Vector2(500, 650), player, Vector2(750, 850), 0.413);
-	
-func _ready():
-	enemy = get_child(0);
-	player = get_child(1);
-	enemy.set_process(false);
-	enemy.set_physics_process(false);
-	animationPlayer = enemy.get_child(2);
-	animationPlayer.play("Intro");
-	set_process(false);
-
-func _on_Animation_Finished(anim_name):
-	if(anim_name == "Intro"):
-		animationPlayer.play("Intro2");
-		time_begin = OS.get_ticks_usec();
-		time_delay = AudioServer.get_time_since_last_mix() + AudioServer.get_output_latency();
-		set_process(true);
-		enemy.set_process(true);
-		enemy.set_physics_process(true);
-		$BGM.play();
-	
-func _process(delta):
-	var time = (OS.get_ticks_usec() - time_begin) / 1000000.0;
-	time -= time_delay;
-	time = max(0, time);
-	
-	if(player == null):
-		get_tree().paused = true;
-	
-	while(listeners_array.size() && time >= listeners_array[0][0]):
-		var callback = funcref(self, listeners_array[0][1]);
-		callback.call_funcv(listeners_array[0][2]);
-		
-		listeners_array.pop_front();
-
-
 func _on_Bounds_body_exited(body):
 	if(body.get_class() == 'Bullet'):
 		body.queue_free();
